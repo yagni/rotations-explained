fdc75cac032408029496746a25bc06f82175e31a
yagni <yagni@users.noreply.github.com>
2015-09-17 23:42:01 -0500

    Pretty happy with look of rotation-ctrl.

--- app/elements/rotation-ctrl/rotation-ctrl.js
+++ app/elements/rotation-ctrl/rotation-ctrl.js
@@ -2,44 +2,15 @@
   Polymer({
     is: 'rotation-ctrl',
     properties: {
-      items: {
-        type: Array,
-        notify: true,
+      showMatrix: {
+        type: Boolean,
+        value: true
       },
-      matrixVisibility: 'visibility: hidden',
-      anglesVisibility: 'visible: none'
+    },
+    toggleView: function() {
+      this.showMatrix = !this.showMatrix;
     },
     ready: function() {
-      this.items = [
-        'Responsive Web App boilerplate',
-        'Iron Elements and Paper Elements',
-        'End-to-end Build Tooling (including Vulcanize)',
-        'Unit testing with Web Component Tester',
-        'Routing with Page.js',
-        'Offline support with the Platinum Service Worker Elements'
-      ];
     }
   });
 })();
-
-//import Ember from 'ember';
-//
-//export default Ember.Component.extend({
-//  //myInit: function() {
-//  //  this._super();
-//  //  this.set('showMatrix', true);
-//  //  this.set('editable', true);
-//  //}.on('init'),
-//  allowClose: true,
-//  editable: true,
-//  showMatrix: true,
-//  classNames: ['matrix'],
-//  actions: {
-//    close: function() {
-//      this.sendAction('close', this.get('matrix'));
-//    },
-//    toggleMode: function() {
-//      this.set('showMatrix',!this.get('showMatrix'));
-//    }
-//  }
-//});
